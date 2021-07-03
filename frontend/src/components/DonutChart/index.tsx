f0a070e018f8bae675fc82c8fb8034bcfa118307
yuri <yuri_resende@hotmail.com>
2021-07-03 20:36:15 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,6 @@
 import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -11,22 +13,43 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //FORMA ERRADA
-    let chartData : ChartData = {labels: [], series: []};
+    const [chartData, setChartData] = useState<ChartData>({labels: [], series: []});
 
-    //FORMA ERRADA
-    //axios.get(BASE_URL + '/sales/amount-by-seller')
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    
+
+    useEffect( () => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then( response => {
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = {labels: myLabels, series: mySeries};
+            
+            setChartData({labels: myLabels, series: mySeries});
 
-            //console.log(response.data);
-            console.log(chartData);
+            
+            // console.log(chartData);
         } );
+    }, []);
+    
+
+    //FORMA ERRADA(chartData recebe dados assincronamente e nao reenderiza grafico rosca na tela)
+    //let chartData : ChartData = {labels: [], series: []};
+
+    //FORMA ERRADA
+    //axios.get(BASE_URL + '/sales/amount-by-seller')
+    // axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //     .then( response => {
+    //         const data = response.data as SaleSum[];
+    //         const myLabels = data.map(x => x.sellerName);
+    //         const mySeries = data.map(x => x.sum);
+
+    //         //chartData = {labels: myLabels, series: mySeries};
+    //         setChartData({labels: myLabels, series: mySeries});
+
+    //         //console.log(response.data);
+    //         console.log(chartData);
+    //     } );
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
